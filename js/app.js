325caf7882db91bbc7d5c9a5904ef7d2e765de45
Haziq Naeem <haziqnaeem36@gmail.com>
2022-02-04 10:58:41 -0500

    render function

--- js/app.js
+++ js/app.js
@@ -3,11 +3,15 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let squares = []
+let turn
+let isWinner
 
 
 /*------------------------ Cached Element References ------------------------*/
+const sq = document.querySelectorAll('.sq')
 
+const statusMessage = document.querySelector('#message')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -15,4 +19,24 @@
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
 
+function init(){
+  squares = [null, null, null, null, null, null, null, null, null]
+  turn = 1
+  isWinner = null
+  render()
+}
+
+function render(){
+  for(i = 0; i < squares.length; i++){
+    if(squares[i] === 1){
+      squares[i].style = 'blue'
+    } else if(squares[i] === -1 ){
+      squares[i].stlye === 'red'
+    } else if (squares[i] === null) {
+      squares[i].stlye = 'green'
+    }
+  }
+  
+}
